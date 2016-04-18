8d951280b7e2a15b05aeba7e5141cbc0665f722b
John Sweitzer <john_sweitzer@thirdwaveinsights.com>
2016-04-18 15:13:39 -0500

    Change deliver-topology:: to topology-truck::

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-include_recipe 'deliver-topology::_default_acceptance'
-include_recipe 'deliver-topology::_default_union'
-include_recipe 'deliver-topology::_default_rehearsal'
-include_recipe 'deliver-topology::_default_delivered'
+include_recipe 'topology-truck::_default_acceptance'
+include_recipe 'topology-truck::_default_union'
+include_recipe 'topology-truck::_default_rehearsal'
+include_recipe 'topology-truck::_default_delivered'
