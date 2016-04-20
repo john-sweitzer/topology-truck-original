ca1f2f5026642ea5aa51295cd61e688d8882c914
John Sweitzer <john_sweitzer@thirdwaveinsights.com>
2016-04-20 14:07:41 -0500

    Fix the stage test in _default_* for delivered and rehearsal stages.

--- recipes/_default_delivered.rb
+++ recipes/_default_delivered.rb
@@ -1,11 +1,11 @@
 #
 # Cookbook Name:: topology-truck
-# Recipe:: _default_acceptance
+# Recipe:: _default_delivered
 #
 # Copyright:: Copyright (c) 2016 ThirdWave Insights, LLC
 # License:: Apache License, Version 2.0
 
 # The acceptance stage requires some set up before processing the phases.
-if node['delivery']['change']['stage'] == 'union'
+if node['delivery']['change']['stage'] == 'delivered'
   include_recipe 'topology-truck::_default_ssh'
 end
--- recipes/_default_rehearsal.rb
+++ recipes/_default_rehearsal.rb
@@ -1,11 +1,11 @@
 #
 # Cookbook Name:: topology-truck
-# Recipe:: _default_acceptance
+# Recipe:: _default_rehearsal
 #
 # Copyright:: Copyright (c) 2016 ThirdWave Insights, LLC
 # License:: Apache License, Version 2.0
 
 # The acceptance stage requires some set up before processing the phases.
-if node['delivery']['change']['stage'] == 'union'
+if node['delivery']['change']['stage'] == 'rehearsal'
   include_recipe 'topology-truck::_default_ssh'
 end
