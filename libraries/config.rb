71078008bf6258750cfa493b02c538c663420365
John Sweitzer <john_sweitzer@thirdwaveinsights.com>
2016-04-22 16:02:08 -0500

    new libraries and approach

---
+++ libraries/config.rb
@@ -0,0 +1,102 @@
+#
+# Cookbook Name:: topo
+#
+# Copyright (c) 2015 ThirdWave Insights, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+require 'chef/data_bag_item'
+require_relative './node'
+
+class Config
+  # Handle topology data from data bag item
+  class ConfigurationParameters
+    @config = {}
+
+
+    # class method to get or create Topo instance
+    def self.get_topo(name, data_bag = 'topologies')
+      unless @topos[name]
+        @topos[name] = load_from_bag(name, name, data_bag)
+
+        return nil unless @topos[name]
+      end
+  
+      @topos[name]
+
+    end
+
+    def self.load_from_bag(name, item, data_bag)
+      begin
+        raw_data = Chef::DataBagItem.load(data_bag, item)
+        raw_data['name'] = item if raw_data # Restore name attribute because of chef bug
+        topo = Topo::Topology.new(name, raw_data.to_hash) if raw_data
+      rescue Net::HTTPServerException => e
+        raise unless e.to_s =~ /^404/
+      end
+      topo
+    end
+
+    def initialize(raw_data)
+
+        @raw_data = raw_data['topology-truck'] || {}
+    
+        #  @attributes = @raw_data['attributes'] || @raw_data['normal'] || {}
+      
+        if @raw_data['provision']
+          
+          
+          driver = @raw_data['provision']['driver'] || ''
+          if driver
+                @driver_type = driver.split(":",2)[0]
+            else
+                @driver_type = "default"
+                end
+          
+          @machine_options = deliver_topo['provision']['machine_options'] || {}
+          else
+            Chef::Log.warn("Unable to find configuration details for topology-truck so cannot deploy topologies")
+            end
+      
+      if @raw_data['stage_topology']
+          stage_topology = deliver_topo['stage_topology'] || {}
+          stage = node['delivery']['change']['stage'] || 'acceptance'
+          @topologies = stage_topology[stage] || []
+          end
+      
+      
+    end
+
+    def driver
+            return @driver if @driver
+            'default'
+    end
+
+
+    def driver_type
+      return @driver_type if @driver_type
+      'default'
+    end
+
+    def machine_options
+            return @machine_options if @ machine_options
+            {}
+    end
+
+    def topologyList
+            return @topologyList if @topologyList
+            []
+    end
+
+    
+  end
+end
--- recipes/provision.rb
+++ recipes/provision.rb
@@ -1,158 +1,79 @@
 #
 # Cookbook Name:: topology-truck
-# Recipe:: _provision_aws
+# Recipe:: provision  (a phase recipe in a Delivery build-cookbook)
 #
 # Copyright:: Copyright (c) 2016 ThirdWave Insights, LLC
 # License:: Apache License, Version 2.0
-
-include_recipe 'chef-sugar'
-
-load_delivery_chef_config
+#
+#
 
 
-# Decrypt the SSH private key Chef provisioning uses to connect to the
-# machine and save the key to disk.
-
-#ssh_key = encrypted_data_bag_item_for_environment(
-#  'provisioning-data', 'ssh_key'
-#)
-#ssh_private_key_path = File.join(node['delivery']['workspace']['cache'], '.ssh')
-#directory ssh_private_key_path
-#file File.join(ssh_private_key_path, "#{ssh_key['name']}.pem") do
-#  sensitive true
-#  content ssh_key['private_key']
-#  owner node['delivery_builder']['build_user']
-#  group node['delivery_builder']['build_user']
-#  mode '0600'
-#end
 
 # Setup up some local variable for frequently used values for cleaner code...
 project = node['delivery']['change']['project']
 stage = node['delivery']['change']['stage']
 
+# Setup local variables for configuration details in the config.json file...
 
-# ...  setup the local variable for the configuration configuration details in the config.json
+ raw_data = node['delivery']['config']['topology-truck']
+ config = Config::ConfigurationParameter.new(raw_data.to_hash) if raw_data
 
-if node['delivery']['config']['topology-truck']
-    
-    deliver_topo = node['delivery']['config']['topology-truck']
-    
-    driver = deliver_topo['provision']['driver'] || ''
-    if driver
-        @driver_type = driver.split(":",2)[0]
-        else
-        @driver_type = "default"
-    end
-    
-    machine_options = deliver_topo['provision']['machine_options'] || {}
-    stage_topology = deliver_topo['stage_topology'] || {}
-    topologies = stage_topology[stage] || []
-    
-    Chef::Log.warn("delivery_topo....    #{deliver_topo}")
-    Chef::Log.warn("driver....           #{driver}")
-    Chef::Log.warn("driver_type....      #{@driver_type}")
-    Chef::Log.warn("machine_options      #{machine_options}")
-    Chef::Log.warn("stage_toplogy....    #{stage_topology}")
-    Chef::Log.warn("topologies....       #{topologies}")
-    
-else
-      Chef::Log.warn("Unable to find configuration details for topology-truck so cannot deploy topologies")
+Chef::Log.warn("raw_data....    #{raw_data}")
+Chef::Log.warn("driver....           #{config.driver()}")
+Chef::Log.warn("driver_type....      #{config.driver_type()}")
+Chef::Log.warn("machine_options      #{config.machine_options()}")
+Chef::Log.warn("topologies....       #{config.topologyList()}")
+
+
+# Initialize the ssh driver after loading it..
+require 'chef/provisioning/ssh_driver'
+with_driver 'ssh'
+
+# Run something in compile phase using delivery chef server
+with_server_config do
+  Chef::Log.info("Doing stuff like topo truck getting data bags from chef server #{delivery_chef_server[:chef_server_url]}")
 end
 
-if @driver_type.downcase === 'aws'
-
-# Load AWS credentials.
-include_recipe "#{cookbook_name}::_aws_creds"
-
-# Initialize the AWS driver after loading it..
-require 'chef/provisioning/aws_driver'
-with_driver driver
-
-end
-
-if @driver_type.downcase === 'ssh'
-    
-    require 'chef/provisioning/ssh_driver'
-    with_driver driver 
-end
-
-# Specify information about our Chef server.
-# Chef provisioning uses this information to bootstrap the machine.
+# Setup info so cheffish/chef provisioning uses delivery chef server
 with_chef_server(
-  Chef::Config[:chef_server_url],
-  client_name: Chef::Config[:node_name],
-  signing_key_filename: Chef::Config[:client_key],
-  ssl_verify_mode: :verify_none,
-  verify_api_cert: false
+  delivery_chef_server[:chef_server_url], 
+  client_name: delivery_chef_server[:options][:client_name],
+  signing_key_filename: delivery_chef_server[:options][:signing_key_filename]
+  # some specific client.rb options can go here, but not ssl_verify_mode
 )
 
-#  The recipe is expecting there to be a list of topologies that need machine
-#  for  each stage of the pipeline.  Source of the topology list is determined
-#  by the details in the config.json file used to configure this pipeline.
-#  When this file contains a 'stage_topology' hash those details are used.
-#  Otherwise the topology details in the attribute file is used.
+# compile-time code here will execute in local chef server context
 
-topology_list = []
+# arbitrary options to go in client.rb on provisioned nodes
+debug_config = "log_level: :info \n"\
+  'verify_api_cert: false'
 
-if topologies
-    # Retrieve the topology details from data bags in the Chef server...
-    topologies.each do |topology_name|
-        
-        Chef::Log.warn("*** TOPOLOGY NAME.............    #{topology_name} ")
-        
-        topology = Topo::Topology.get_topo(topology_name)
-        
-        #raw_data = Chef::DataBagItem.load('topologies', topology_name)
-        #topo = raw_data
-        
-        if topology
-            topology_list.push(topology)
-        else
-            Chef::Log.warn("Unable to find topology #{topology_name} so cannot privision any nodes.")
-        end
-end
-
-stage_aws_mach_opts = node[project][stage][@driver_type.downcase]['config']['machine_options']
-
-
-# Now we are ready to provision the nodes in each of the topologies
-topology_list.each  do |topology|
-    
-  topology_name = topology.name() 
-
-  # When there are provisioning details in the topology data bag, extract them
-  # and load the values into a structure with symbols rather than string hashes
-  if topology['provisioning'] && topology['provisioning']['ssh']
-    mach_opts = topology['provisioning']['ssh']['config']['machine_options']
-    stage_aws_mach_opts['transport_options'] = mach_opts['transport_options']
-    Chef::Log.warn("*** MACHINE OPTIONS.............    #{mach_opts.inspect}")
-  end
-
-  # Provision each node in the current topology...
-  topology['nodes'].each do |node_details|
-    Chef::Log.warn(
-      '*** TOPOLOGY NODE(S).............   ' \
-      " #{topology_name} NODE:  #{node_details['name']}"
-    )
-
-    # Prepare a new machine / node for a chef client run...
-    machine node_details['name'] do
-      action [:setup]
-      converge false
-      add_machine_options bootstrap_options: {
-        key_name: ssh_key['name'],
-        key_path: ssh_private_key_path
+# actual provisioning using delivery chef server
+machine 'debugnode' do
+  action [:setup]
+  chef_environment '_default'
+  converge false
+  run_list [ 'recipe[yum::default]']
+  machine_options(
+    transport_options: {
+      'ip_address' => '10.0.1.2',
+      'username' => 'vagrant',
+      'ssh_options' => {
+        'password' => 'vagrant'
       }
-      add_machine_options stage_aws_mach_opts
+    },
+    convergence_options: {
+      ssl_verify_mode: :verify_none,
+      chef_config: debug_config
+    }
+  )
+end
+
+ruby_block "do stuff like delivery truck" do
+  block do
+    # run stuff using delivery chef server in converge phase
+    with_server_config do
+      Chef::Log.info("Doing stuff like delivery truck pinning envs with chef server #{delivery_chef_server[:chef_server_url]}")
     end
   end
 end
-
-current_server = chef_server
-ruby_block 'reset chef server' do
-    block do
-        current_server.unload_server_config
-    end
-end
-end
-
