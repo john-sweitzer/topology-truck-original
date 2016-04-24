252c627fb83b61f863e220d46a1c246d866ad5ed
John Sweitzer <john_sweitzer@thirdwaveinsights.com>
2016-04-24 18:36:20 -0500

    restore call to Configuration Parameter

--- recipes/provision.rb
+++ recipes/provision.rb
@@ -18,6 +18,8 @@ stage = node['delivery']['change']['stage']
     raw_data = {}
     raw_data['topology-truck'] = node['delivery']['config']['topology-truck']
 
+config = Topo::ConfigurationParameter.new(raw_data.to_hash,stage) if raw_dat:a
+
 Chef::Log.warn("raw_data....         #{raw_data}")
 Chef::Log.warn("raw_data.to_hash...  #{raw_data.to_hash}")
 Chef::Log.warn("driver....           #{config.driver()}")
