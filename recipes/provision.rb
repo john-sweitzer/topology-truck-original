6372dd7740263d3e872b21d27f7abfb2e02aa4dd
John Sweitzer <john_sweitzer@thirdwaveinsights.com>
2016-04-27 08:56:55 -0500

    Create a dummy file name...

--- recipes/provision.rb
+++ recipes/provision.rb
@@ -28,6 +28,8 @@ Chef::Log.warn("topologies....       #{config.topologyList()}")
 
 # Decrypt the SSH private key Chef provisioning uses to connect to the
 # machine and save the key to disk.
+ssh_key = {}
+ssh_key['name'] = 'noFileToSetup' if config.driver_type != 'aws'
 ssh_key = encrypted_data_bag_item_for_environment('provisioning-data', 'ssh_key') if config.driver_type == 'aws'
 ssh_private_key_path = File.join(node['delivery']['workspace']['cache'], '.ssh') if config.driver_type == 'aws'
 directory ssh_private_key_path if config.driver_type == 'aws'
